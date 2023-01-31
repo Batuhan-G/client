b3f8d26c45261e5a7926f86ac2ff9824f319bc65
batuhangoren99@hotmail.com <batuhangoren99@hotmail.com>
2023-02-01 01:58:22 +0300

    Cart design added

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown'
 import SearchIcon from '@mui/icons-material/Search'
 import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined'
@@ -6,8 +6,12 @@ import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined'
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined'
 import { Link } from 'react-router-dom'
 import "./Navbar.scss"
+import Cart from '../Cart/Cart'
 
 const Navbar = () => {
+
+  const [open, setOpen] = useState(false)
+
   return (
     <div className='navbar'>
       <div className="wrapper">
@@ -50,14 +54,14 @@ const Navbar = () => {
             <SearchIcon />
             <PersonOutlineOutlinedIcon />
             <FavoriteBorderOutlinedIcon />
-            <div className="cartIcon">
+            <div className="cartIcon" onClick={() => setOpen(!open)}>
               <ShoppingCartOutlinedIcon />
               <span>0</span>
             </div>
           </div>
         </div>
       </div>
-
+      {open && <Cart />}
     </div>
   )
 }
