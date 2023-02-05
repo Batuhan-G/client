0d64abe292be2aac1333d958080fecf3826dc8fa
batuhangoren99@hotmail.com <batuhangoren99@hotmail.com>
2023-02-05 18:08:08 +0300

    RestAPI filtering and fetch a single product

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,55 +1,24 @@
-import React from 'react'
-import Card from '../Card/Card'
-import './List.scss'
+import React from "react";
+import "./List.scss";
+import Card from "../Card/Card";
+import useFetch from "../../hooks/useFetch";
 
-const List = () => {
-
-    const data = [
-        {
-            id: 1,
-            img: "https://picsum.photos/id/237/200/300",
-            img2: "https://picsum.photos/id/236/200/300",
-            title: "Title1",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 2,
-            img: "https://picsum.photos/id/235/200/300",
-            img2: "https://picsum.photos/id/234/200/300",
-            title: "Title2",
-            isNew: true,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 3,
-            img: "https://picsum.photos/id/233/200/300",
-            img2: "https://picsum.photos/id/232/200/300",
-            title: "Title2",
-            isNew: false,
-            oldPrice: 19,
-            price: 12,
-        },
-        {
-            id: 4,
-            img: "https://picsum.photos/id/231/200/300",
-            img2: "https://picsum.photos/id/230/200/300",
-            title: "Title2",
-            isNew: false,
-            oldPrice: 19,
-            price: 12,
-        },
-    ]
+const List = ({ subCats, maxPrice, sort, catId }) => {
+  const { data, loading, error } = useFetch(
+    `/products?populate=*&[filters][categories][id]=${catId}${subCats.map(
+      (item) => `&[filters][sub_categories][id][$eq]=${item}`
+    )}&[filters][price][$lte]=${maxPrice}&sort=price:${sort}`
+  );
 
   return (
-    <div className='list'>
-        {data?.map(item => (
-            <Card item={item} key= {item.id} />
-        ))}
+    <div className="list">
+      {error
+      ? "Something went wrong!"
+      : loading
+        ? "loading"
+        : data?.map((item) => <Card item={item} key={item.id} />)}
     </div>
-  )
-}
+  );
+};
 
-export default List
+export default List;
